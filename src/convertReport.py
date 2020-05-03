612ea1d55192c23b40b92454a0d1a2a47baecdf9
Armin Sabouri <armins88@gmail.com>
2020-05-03 11:33:59 -0400

    Formated all csv's

--- src/convertReport.py
+++ src/convertReport.py
@@ -4,6 +4,7 @@ import pandas as pd
 from pathlib import Path
 import glob, os
 from datetime import datetime
+import json
 
 current_time = datetime.now()
 formatedTime = str(current_time.month) + "-" + str(current_time.day) + "-" + str(current_time.year)
@@ -22,21 +23,28 @@ def get_date_from_name(name):
     ##Format on mass.gov is -- COVID-19-Dashboard-Files-mm-dd-yyyy -> extract the date
     return "-".join(name.split('-')[4:])
 
-def get_csv_names():
-    os.chdir("reports/COVID-19-Dashboard-Files-"+formatedTime)
-    return [f for f in glob.glob("*.csv")]
+def format_csv_file_name(name):
+    return name.replace(' ', '').replace('.csv', '')
+
+def get_csv_names(path):
+    old_dir = os.getcwd()
+    os.chdir(old_dir + '/reports/' + path)
+    sorted_dirs =  [f for f in glob.glob("*.csv")] 
+    os.chdir(old_dir)
+    return sorted_dirs
 
 def generate_reports():
     report_obj = {}
-    for report in get_csv_names():
-        report_obj[report] = generate_report(report)
+    for report_path in get_report_files_names():
+        formated_date = get_date_from_name(report_path)
+        report_obj[formated_date] = {}
+        for report_aspect in get_csv_names(report_path):
+            report_obj[formated_date][format_csv_file_name(report_aspect)] = generate_report(report_path, report_aspect)
+    return str(json.dumps(report_obj)).replace('NaN', '0')
 
-def generate_report(file_name):
-
-    reports = []
-
-    df = pd.read_csv(file_name)
-    return df.to_json()
+def generate_report(path, file_name):
+    df = pd.read_csv('./reports/' + path + '/' +file_name)
+    return df.to_dict('records')
 
 def pickle_report_object(obj):
    pickle.dump(obj, 'pickle/current_report') 
