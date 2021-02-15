bcb516a717cf3a61d2b5af43910f7ac6d31802ca
Armin Sabouri <armin@team.casa>
2021-02-15 13:43:59 -0500

    general update

--- src/convertReport.py
+++ src/convertReport.py
@@ -1,50 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3.8
 
 import pandas as pd
 from pathlib import Path
 import glob, os
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 
-current_time = datetime.now()
-formatedTime = str(current_time.month) + "-" + str(current_time.day) + "-" + str(current_time.year)
-
-
-def get_report_files_names():
-    old_dir = os.getcwd()
-    os.chdir(old_dir + '/reports')
-    sorted_dirs = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
- 
-    os.chdir(old_dir)
-
-    return  sorted_dirs
-
-def get_date_from_name(name):
-    ##Format on mass.gov is -- COVID-19-Dashboard-Files-mm-dd-yyyy -> extract the date
-    return "-".join(name.split('-')[4:])
-
-def format_csv_file_name(name):
-    return name.replace(' ', '').replace('.csv', '')
-
-def get_csv_names(path):
-    old_dir = os.getcwd()
-    os.chdir(old_dir + '/reports/' + path)
-    sorted_dirs =  [f for f in glob.glob("*.csv")] 
-    os.chdir(old_dir)
-    return sorted_dirs
+current_time = datetime.now() - timedelta(days=1)
+formatedTime = str(current_time.strftime('%b')) + "-" + str(current_time.day) + "-" + str(current_time.year)
 
 def generate_reports():
     report_obj = {}
-    for report_path in get_report_files_names():
-        formated_date = report_path
-        report_obj[formated_date] = {}
-        for report_aspect in get_csv_names(report_path):
-            report_obj[formated_date][format_csv_file_name(report_aspect)] = generate_report(report_path, report_aspect)
-    return str(json.dumps(report_obj)).replace('NaN', '0')
+    df = pd.ExcelFile('./reports/'+formatedTime+'.xlsx', engine='openpyxl')
+    sheets = df.sheet_names
+    jsonDump = {}
+    for sheet in sheets:
+        parsed_sheet = df.parse(sheet)
+        jsonDump[sheet] = parsed_sheet.to_dict(orient='records')
+    def dateConverter(o):
+        if isinstance(o, datetime):
+            return o.__str__()
+    return str(json.dumps(jsonDump, default=dateConverter)).replace('NaN', '0')
 
-def generate_report(path, file_name):
-    df = pd.read_csv('./reports/' + path + '/' +file_name)
-    return df.to_dict('records')
+# def write_report():
+#     report = generate_reports()
 
-def pickle_report_object(obj):
-   pickle.dump(obj, 'pickle/current_report') 
+print(generate_reports())
--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3.8
 
 
 from flask import Flask
@@ -10,10 +10,5 @@ app = Flask(__name__)
 def get():
      return generate_reports()
 
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080')
-
-
-
